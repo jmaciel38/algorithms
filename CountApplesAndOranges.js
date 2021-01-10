f438b356e1b1cfbbd1094c4917337ee381f6e0dc
5k1nnym4n <josemaciel30@gmail.com>
2021-01-09 22:02:07 -0300

    CountApplesAndOranges.js v0.2 09.01.2021

--- CountApplesAndOranges.js
+++ CountApplesAndOranges.js
@@ -1,42 +1,106 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    if(s < 1 || t < 1 || a < 1 || b < 1 || apples < 1 || oranges < 1) return 0;
-    if(s > 10**5 || t > 10**5 || a > 10**5 || b > 10**5 || apples > 10**5 || oranges > 10**5) return 0;
+    if(s < 1 || t < 1 || a < 1 || b < 1) return 0;
+    if(s > 10**5 || t > 10**5 || a > 10**5 || b > 10**5) return 0;
+    if(typeof apples != 'object' || typeof oranges != 'object') return 0;
+    if(apples.length < 1 || apples.length > 10**5) return 0;
+    if(oranges.length < 1 || oranges.length > 10**5) return 0;
     if(a >= s || s >= t || t >= b) return 0;
-    let appleTreeDist;
-    let orangeTreeDist;
-    let houseSpace = t - s;
-    let result = [1, 1];
+    let countApples = 0;
+    let countOranges = 0;
+    for(let i = 0; i < apples.length; i++){
+        if(apples[i] < -(10**5) || apples[i] > 10**5) return 0;
+        let distApple = a + apples[i];
+        if( distApple >= s) countApples++;
+    }
+    for(let i = 0;  i < oranges.length; i++){
+        if(oranges[i] < -(10**5) || oranges[i] > 10**5) return 0;
+        let distOranges = b + oranges[i];
+        if(distOranges <= t) countOranges++;
+    }
+    let result = [countApples, countOranges];
+    for(let i = 0; i < result.length; i ++) {
+        console.log(result[i]);
+    }
     return result;
 }
 /* 
-teste de parametros ok
+teste 01: de parametros ok
 */
-testS_01 = 2; testT_01 = 3; testA_01 = 1; testB_01 = 4; testApples_01 = 1; testOranges_01 = 1;
-testResult01 = countApplesAndOranges(testS_01, testT_01, testA_01, testB_01, testApples_01, testOranges_01);
+testS_01 = 7; testT_01 = 11; testA_01 = 5; testB_01 = 15;
+testApples_01 = [-2, 2, 1]; testOranges_01 = [5, -6];
+testResult01 = countApplesAndOranges(
+	testS_01, testT_01, testA_01, testB_01, testApples_01, testOranges_01
+);
 
 /* 
-teste de Constraints 1 <= s, t, a, b, apples, oranges
+teste 02: de Constraints 1 <= s, t, a, b
 */
-testS_02 = 0; testT_02 = 3; testA_02 = 1; testB_02 = 4; testApples_02 = 1; testOranges_02 = 1;
-testResult02 = countApplesAndOranges(testS_02, testT_02, testA_02, testB_02, testApples_02, testOranges_02);
+testS_02 = 0; testT_02 = 11; testA_02 = 5; testB_02 = 15;
+testApples_02 = [-2, 2, 1]; testOranges_02 = [5, -6];
+testResult02 = countApplesAndOranges(
+	testS_02, testT_02, testA_02, testB_02, testApples_02, testOranges_02
+);
 
 /* 
-teste de Constraints s, t, a, b, apples, oranges <= 10**5
+teste 03: de Constraints s, t, a, b <= 10**5
 */
-testS_03 = 0; testT_03 = 3; testA_03 = 1; testB_03 = 4; testApples_03 = 100001; testOranges_03 = 1;
-testResult03 = countApplesAndOranges(testS_03, testT_03, testA_03, testB_03, testApples_03, testOranges_03);
+testS_03 = 7; testT_03 = 100001; testA_03 = 5; testB_03 = 15;
+testApples_03 = [-2, 2, 1]; testOranges_03 = [5, -6];
+testResult03 = countApplesAndOranges(
+	testS_03, testT_03, testA_03, testB_03, testApples_03, testOranges_03
+);
 
 /* 
-teste 01 de Constraints s, t, a, b, apples, oranges <= 10**5
+teste 04: apples, oranges tipo array
 */
-testS_04 = 0; testT_04 = 0; testA_04 = 1; testB_04 = 4; testApples_04 = 1; testOranges_04 = 1;
-testResult04 = countApplesAndOranges(testS_04, testT_04, testA_04, testB_04, testApples_04, testOranges_04);
+testS_04 = 7; testT_04 = 11; testA_04 = 5; testB_04 = 15;
+testApples_04 = 'erro'; testOranges_04 = [5, -6];
+testResult04 = countApplesAndOranges(
+	testS_04, testT_04, testA_04, testB_04, testApples_04, testOranges_04
+);
 
+/* 
+teste 05: a >= s || s >= t || t >= b
+*/
+testS_05 = 7; testT_05 = 11; testA_05 = 9; testB_05 = 15;
+testApples_05 = [-2, 2, 1]; testOranges_05 = [5, -6];
+testResult05 = countApplesAndOranges(
+	testS_05, testT_05, testA_05, testB_05, testApples_05, testOranges_05
+);
 
+/* 
+teste 06: d < -10**5
+*/
+testS_06 = 7; testT_06 = 11; testA_06 = 5; testB_06 = 15;
+testApples_06 = [-100002, 2, 1]; testOranges_06 = [5, -6];
+testResult06 = countApplesAndOranges(
+	testS_06, testT_06, testA_06, testB_06, testApples_06, testOranges_06
+);
+
+/* 
+teste 07: d > 10**5
+*/
+testS_07 = 7; testT_07 = 11; testA_07 = 5; testB_07 = 15;
+testApples_07 = [-2, 2, 1]; testOranges_07 = [100005, -6];
+testResult07 = countApplesAndOranges(
+	testS_07, testT_07, testA_07, testB_07, testApples_07, testOranges_07
+);
+
+/* input de exemplo
+s, t: 7 11
+a, b: 5 15
+apples, oranges: 3, 2
+apples positions: -2 2 1
+oranges positions: 5 -6
+*/
 
 console.log({
     testResult01,
     testResult02,
     testResult03,
     testResult04,
+    testResult05,
+    testResult06,
+    testResult07,
 });
+testApples_07.length
