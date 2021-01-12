7be1fa61a8ed8b109b49c3c5b9a7dec4d274aec0
5k1nnym4n <josemaciel30@gmail.com>
2021-01-12 20:35:00 -0300

    BetweenTwoSets v0.2 12.01.2021

--- BetweenTwoSets/BetweenTwoSets.js
+++ BetweenTwoSets/BetweenTwoSets.js
@@ -9,20 +9,27 @@ function main(fileName) {
     var fs = require('fs');
     inputString = fs.readFileSync(fileName).toString().split('\n')
     .map(str => str.replace(/\s*$/, ''));
-    const ab = readLine(inputString, currentLine).split(' ');
+    const ab = readLine().split(' ');
     const aLength = parseInt(ab[0], 10);
     const bLength = parseInt(ab[1], 10);
-    const aArr = readLine().split(' ');
-    const bArr = readLine().split(' ');
+    const aArr = readLine().split(' ').map(aArrTemp => parseInt(aArrTemp, 10));
+    const bArr = readLine().split(' ').map(bArrTemp => parseInt(bArrTemp, 10));
     const total = getTotalX(aLength, bLength, aArr, bArr)
 }
 
 function getTotalX(aLength, bLength, aArr, bArr) {
+    if(aArr.length < 1 || aArr.length > 10) return 0;
+    if(bArr.length < 1 || bArr.length > 10) return 0;
+    if(Math.min(...aArr) < 1 || Math.max(...aArr) > 100) return 0;
+    if(Math.min(...bArr) < 1 || Math.max(...bArr) > 100) return 0;
+
     console.log({
         aLength,
         bLength,
         aArr,
-        bArr
+        bArr,
+        aArrLength: aArr.length,
+        bArrLength: bArr.length,
     });
     return 1;
 }
