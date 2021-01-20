806984f51bedf7d7163147868453b43b77e611e0
5k1nnym4n <josemaciel30@gmail.com>
2021-01-20 14:43:37 -0300

    SubarrayDivision v1.0 20.01.2021

--- SubarrayDivision/subarrayDivision.js
+++ SubarrayDivision/subarrayDivision.js
@@ -9,7 +9,8 @@ const main = file => {
     .map(str => str.replace(/\s*$/, ''));
     const n = parseInt(readLine(inputString, currentLine).trim(), 10);
     currentLine++;
-    const s = readLine(inputString, currentLine).replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+    const s = readLine(inputString, currentLine).replace(/\s+$/g, '').split(' ')
+    .map(sTemp => parseInt(sTemp, 10));
     currentLine++;
     const dm = readLine(inputString, currentLine).replace(/\s+$/g, '').split(' ');
     const d = parseInt(dm[0], 10);
@@ -21,9 +22,21 @@ const birthday = (s, d, m) => {
     if(s.length < 1 || s.length > 100) return 0;
     if(d < 1 || d > 31) return 0;
     if(m < 1 || m > 12) return 0;
-    //if(s[1] < 1 || s[1] > 5) return 0;
-    
-    console.log ({s, d, m});
+    let count = 0;
+    for(let i = 0; i < s.length; i ++) {
+        if(s[i] < 1 || s[i] > 5) return 0;
+        let sum = 0;
+        let whileTest = 0;
+        while(whileTest < m) {
+            sum += s[(i + whileTest)];
+            whileTest++;
+        }
+        if(sum == d) count++;
+    }
+    console.log ({s, d, m,count});
+    return count;
 }
 
 main('input00.txt');
+main('input01.txt');
+main('input02.txt');
