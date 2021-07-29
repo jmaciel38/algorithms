83156bd0b81cb49a3656b7a5ed1c3e8ca1a61add
jmaciel38 <josemaciel30@gmail.com>
2021-07-29 09:59:25 -0300

    5° hackerrank Java II 29.07.2021

--- java/05-java-output-formatting-testcases/JavaOutputFormat.java
+++ java/05-java-output-formatting-testcases/JavaOutputFormat.java
@@ -6,18 +6,30 @@ import java.util.Scanner;
 public class JavaOutputFormat {
 
   public static void main(String[] args) throws FileNotFoundException {
-    File file = new File("./input/input00.txt");
+    File file = new File("./input/input01.txt");
     Scanner sc = new Scanner(file);
     System.out.println("================================");
-    for(int i=0;i<3;i++){
-      String s1=sc.next();
-      int x=sc.nextInt();
+    String val;
+    String res = "";
+    for(int i = 0; i < 3; i++){
+      String s1 = sc.next();
+      int x = sc.nextInt();
       int diff =  15 - s1.length();
-      //Complete this line
-      System.out.println("string length is: "+s1.length());//10 is the length of javatpoint string
-      System.out.println("Diff is: "+diff);
+      res += s1;
+      for(int j = 0; j < diff; j++) {
+        res += " ";
+      }
+      if (x < 10) {
+        val = "00" + x;
+      } else if (x < 100) {
+        val = "0" + x;
+      } else {
+        val = "" + x;
+      }
+      res += val;
+      System.out.println(res);
+      res = "";
     }
     System.out.println("================================");
-
   }
 }
