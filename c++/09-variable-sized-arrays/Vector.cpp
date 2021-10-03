552e9e81b968628f483d9dc7b6d9286a5cd76a50
jmaciel38 <josemaciel30@gmail.com>
2021-10-03 16:34:23 -0300

    Start of 09 chalenge C++ hackerrank 03.10.2021

---
+++ c++/09-variable-sized-arrays/Vector.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  /**
+   * https://www.codeguru.com/cplusplus/c-tutorial-a-beginners-guide-to-stdvector-part-1/
+   * std::vector<int> v;    // declares a vector of integers
+   */
+  return 0;
+}
