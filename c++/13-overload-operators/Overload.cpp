c1433f0880088eb1fbf6f208bc8508ced5a5500d
jmaciel38 <josemaciel30@gmail.com>
2021-10-31 12:16:43 -0300

    13-overload-operators 31.10.2021

---
+++ c++/13-overload-operators/Overload.cpp
@@ -0,0 +1,73 @@
+//Operator Overloading
+
+#include<iostream>
+#include <fstream>
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+/**
+ * [operator description]
+ *
+ * Estudar o uso dos operadores '&' e 'const' neste caso.
+ * Este a compilação não funcionou assim como o 12.
+ */
+Complex operator + (const Complex& A , const Complex& B) {
+  return { A.a + B.a, A.b + B.b };
+}
+
+ostream& operator << (ostream& os, const Complex& C) {
+  return os << C.a << (C.b > 0 ? '+' : '-') << 'i' << C.b;
+}
+
+int main()
+{
+  std::ifstream file;
+  std::ifstream output;
+  file.open("input/input00.txt");
+  output.open("output/output00.txt");
+  if ( file.is_open() && output.is_open()) {
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    cout<<z<<endl;
+  } else {
+    std::cout << "file read error.\n\n";
+    file.close();
+    cout << "This is the line number " << __LINE__;
+    cout << " of file " << __FILE__ << ".\n";
+    cout << "Its compilation began " << __DATE__;
+    cout << " at " << __TIME__ << ".\n";
+    cout << "The compiler gives a __cplusplus value of " << __cplusplus;
+    return 1;
+  }
+}
