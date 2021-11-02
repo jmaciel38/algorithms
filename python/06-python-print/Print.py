1a902516f9af22927658dfdeff7b8f2a0f274283
jmaciel38 <josemaciel30@gmail.com>
2021-11-02 15:15:29 -0300

    06-python-print 02.11.2021

---
+++ python/06-python-print/Print.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    # n = int(raw_input())
+    f = open("./input/input00.txt", "r")
+    n = int(f.readline())
+    for x in range(n):
+        print (x+1, sep=' ', end='')
