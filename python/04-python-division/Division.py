e1135c963ee86f705d047eb1a036a25f929f7066
jmaciel38 <josemaciel30@gmail.com>
2021-11-02 14:40:46 -0300

    04-python-division 02.11.2021

---
+++ python/04-python-division/Division.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    # a = int(raw_input())
+    # b = int(raw_input())
+    f = open("./input/input00.txt", "r")
+    a = int(f.readline())
+    b = int(f.readline())
+    print(a // b)
+    print(a / b)
