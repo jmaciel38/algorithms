9b28a63fcfa2941ca7a9a8804b85f621678a0a65
jmaciel38 <josemaciel30@gmail.com>
2021-11-15 11:16:19 -0300

    19-text-alignment 15.11.2021

---
+++ python/19-text-alignment/TextAlign.py
@@ -0,0 +1,24 @@
+def txt_align(t):
+    thickness = int(t) #This must be an odd number
+    c = 'H'
+    #Top Cone
+    for i in range(thickness):
+        print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+    #Top Pillars
+    for i in range(thickness+1):
+        print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+    #Middle Belt
+    for i in range((thickness+1)//2):
+        print((c*thickness*5).center(thickness*6))
+    #Bottom Pillars
+    for i in range(thickness+1):
+        print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+    #Bottom Cone
+    for i in range(thickness):
+        print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+if __name__ == '__main__':
+    print("OUT 00 _________________________________")
+    f0 = open("./input/input00.txt", "r")
+    s0 = f0.readline()
+    txt_align(s0)
